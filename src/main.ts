07c840eac705a9884bb3497c259e56fb0b2420e5
danilkolesnik <apparentlysolved@gmail.com>
2025-06-24 19:23:29 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: ['http://localhost:3000','http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
