c3d681854b72e6dec7b9d29bdfdde7d2647757fd
danilkolesnik <apparentlysolved@gmail.com>
2025-07-25 12:32:15 +0300

    fix: modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,16 +8,16 @@ import i18n from "./i18n"
 
 function App() {
   const lang = localStorage.getItem('lang') || 'en';
-  const newUser = localStorage.getItem('new')
+  // const newUser = localStorage.getItem('new')
   i18n.changeLanguage(lang);
 
-  useEffect(() =>{
-    if(!newUser){
-      localStorage.setItem('new', 'true')
-      return
-    }
-    localStorage.setItem('new', 'false')
-  },[])
+  // useEffect(() =>{
+  //   if(!newUser){
+  //     localStorage.setItem('new', 'true')
+  //     return
+  //   }
+  //   localStorage.setItem('new', 'false')
+  // },[])
   return (
     <Routes>
       <Route path="/" element={<Main />}/>
